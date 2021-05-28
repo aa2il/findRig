3cd94e71b35a0b4283242f1a65b8a23dd2f80c67
aa2il <jattili@yahoo.com>
2021-05-27 18:28:19 -0700

    Added license

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Find Rig
 
-Determine rig type attached to usb port.
+Determine rig type attached to usb port.  This is used by start-up scripts to set up accordingly.
+The following rigs are supported:
+    - Yaesu FTdx3000
+    - Yaesu FT991a
+    - Kenwood TS850
+    - ICOM 9700
+    - ICOM 706
+    
--- findRig.py
+++ findRig.py
@@ -1,9 +1,24 @@
 #! /usr/bin/python3
-
-# Find Rig - J.B.Attili - 2019
-
-# Script to determine rig type attached to a serial port.
-
+############################################################################
+#
+# Find Rig - Rev 1.0
+# Copyright (C) 2021 by Joseph B. Attili, aa2il AT arrl DOT net
+#
+# Script to determine rig type attached to a serial port and do any common
+# inits.
+#
+############################################################################
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
 ############################################################################
 
 import sys
@@ -30,7 +45,8 @@ try:
 except:
     files=[]
 
-#print('Files=',files)    
+if VERBOSITY>0:
+    print('Files=',files)    
 
 # Sift through list of usb ports
 for f in files:
@@ -38,25 +54,29 @@ for f in files:
     if VERBOSITY>0:
         print('\n'+port)
 
-    # The modern rigs have been set to operate at 38400 but the old
-    # TS850 can only go up to 4800 bps
+    # The modern rigs have been set to operate at 38400 but my 
+    # old TS850 can only go up to 4800 bps
     for baud in [38400,4800]:
 
         # Try to illicit a response from a rig on this port
         rig=try_port(port,baud,VERBOSITY)
         if rig:
 
-            # Found it - print out rig type and exit
+            # Found it - print out rig type, do any inits and exit
             print(rig[1])
-            if rig[1]=='IC9700' and True:
+            sock = rig[2]
+            if rig[1]=='IC9700':
                 # Set time
                 #sock = direct_connect(0,0)
-                sock = rig[2]
                 sock.set_date_time()
-            elif rig[1]=='FT991a' and True:
+            elif rig[1]=='FTdx3000':
+                # Make sure full-power and ant tuner is on
+                sock.set_power(99)
+                sock.tuner(1)
+                sock.get_response('BY;EX177100;')         # Make sure max TX is also set
+            elif rig[1]=='FT991a':
                 # Turn off split mode - this rig seems to get into split quite a bit
                 #print('Hey')
-                sock = rig[2]
                 sock.split_mode(0)
                 
             sys.exit(0)
